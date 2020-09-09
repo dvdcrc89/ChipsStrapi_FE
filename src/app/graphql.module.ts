286722d1a54e3ebb99cce2e43507af40fc62b2f9
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-09 16:49:07 +0100

    Adds graphql Authorization

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,11 +3,23 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import { environment } from 'src/environments/environment';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context'
+import { GC_AUTH_TOKEN } from './service/auth-service/constants';
 
 const uri = `${environment.url}/graphql`; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
+  const token = localStorage.getItem(GC_AUTH_TOKEN);
+  const auth = setContext((operation, context) => ({
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+  }));
+
+  const link = ApolloLink.from([auth, httpLink.create({ uri })]);
+
   return {
-    link: httpLink.create({uri}),
+    link,
     cache: new InMemoryCache(),
   };
 }
