2c36ffb2251d7f16b874b2079fc1ca30c9488483
dvdcrc89 <dvdcrc89@gmail.com>
2020-09-11 21:39:49 +0100

    Formatting

--- src/app/add-job/add-job.component.ts
+++ src/app/add-job/add-job.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray, Form, AbstractControl } from '@angular/forms';
-import { Jobs, Restaurant, BusinessUser, ShiftDateInput, MutationCreateJobArgs, CreateJobPayload, JobInput, CreateShiftDatePayload } from 'src/types/schema';
-import { AuthService } from '../service/auth-service/auth.service';
-import { DocumentNode } from 'graphql';
-import gql from 'graphql-tag';
-import { Apollo } from 'apollo-angular';
-import { filter, switchMap } from 'rxjs/operators';
-import { ApolloQueryResult } from 'apollo-client';
-import * as moment from 'moment';
+import { Apollo }             from 'apollo-angular';
+import { ApolloQueryResult }  from 'apollo-client';
+import { AuthService }        from '../service/auth-service/auth.service';
+import { Component, OnInit }  from '@angular/core';
+import { DocumentNode }       from 'graphql';
+import { FetchResult }        from 'apollo-link';
+import { filter, switchMap }  from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators, FormArray, AbstractControl, ValidationErrors } from '@angular/forms';
+import { Observable }         from 'rxjs';
+import { Restaurant, BusinessUser, CreateShiftDatePayload } from 'src/types/schema';
+import * as moment            from 'moment';
+import gql                    from 'graphql-tag';
+import { UserInfo } from 'os';
 
+export type ShiftDateGroup = {
+  Date:     (string | ((control: AbstractControl) => ValidationErrors))[],
+  StartAt:  (string | ((control: AbstractControl) => ValidationErrors))[],
+  EndAt:    (string | ((control: AbstractControl) => ValidationErrors))[]
+}
 
+export type CreateShiftDateWithPayload = {
+    createShiftDate: CreateShiftDatePayload
+}
 /** 
  * TO DO
  * Move logic inside service ?
@@ -64,49 +75,64 @@ mutation CreateShiftDate($shift_date: ShiftDateInput) {
 }`
 
 @Component({
-  selector: 'app-add-job',
+  selector: 'add-job',
   templateUrl: './add-job.component.html',
   styleUrls: ['./add-job.component.scss']
 })
 export class AddJobComponent implements OnInit {
   
-  addForm: FormGroup;
-  values: Jobs;
-  didFail: boolean = false;
-  loading: boolean;
-  private restaurant: Restaurant;
+  /** Reactive Form */
+  public addForm: FormGroup;
 
+  /**Server comunication has failed */
+  public didFail: boolean = false;
+  
+  /** Data is loading */
+  public loading: boolean;
+
+  /** Restaurant  of add job component */
+  private restaurant: Pick<Restaurant, "name" | "id">;
+
+  /**
+   * Creates an instance of add job component.
+   * @param authService 
+   * @param fb 
+   * @param apollo 
+   */
   constructor(
     private authService: AuthService, 
     public fb: FormBuilder,
     private apollo: Apollo) { }
 
+  
+  /** Form Submit */
   onSubmit(){
     this.runMutation();
   }
 
+  /** Gets shift dates AbstractControl Array */
   get shift_dates(): AbstractControl[] {
     return (this.addForm.get('shift_date') as FormArray).controls
   }
 
+  /** Gets Shifts as Form Array */
   get shiftsAsArray(): FormArray {
     return this.addForm.get('shift_date') as FormArray
   }
 
-  private shiftDateGroup: any  = {
-    Date: ['',Validators.required],
-    StartAt: ['',Validators.required],
-    EndAt: ['',Validators.required]
+  /** Shift date Form Group */
+  private shiftDateGroup: ShiftDateGroup  = {
+    Date:     [ '', Validators.required ],
+    StartAt:  [ '', Validators.required ],
+    EndAt:    [ '', Validators.required ]
   };
 
   ngOnInit(): void {
-    console.log(moment);
     this.loading = true;
     this.authService._user
     .pipe(
       filter(user => user),
-      switchMap((user) =>{
-        const {business_user} = user;
+      switchMap(({ business_user } : { business_user: number }) =>{
         if(!business_user){
           throw new Error('You cant add jobs, Please sign as bussiness')
         }
@@ -114,7 +140,7 @@ export class AddJobComponent implements OnInit {
       })
     )
     .subscribe((result: ApolloQueryResult<{businessUser:BusinessUser}>) =>{
-      this.loading = false;
+      this.loading    = false;
       this.restaurant = result.data.businessUser.restaurant;
       this.initForm(this.restaurant.id);
     },
@@ -124,29 +150,47 @@ export class AddJobComponent implements OnInit {
 
   }
 
+  /**
+   * Init Add Job Form
+   * @param restaurantID 
+   */
   initForm(restaurantID: string){
     this.addForm = this.fb.group(
       {
-        restaurant: [restaurantID, Validators.required],
-        Type: ['', Validators.required],
-        position: ['', Validators.required],
-        description: ['', Validators.required],
-        payPerHour: ['', Validators.required],
-        shift_date: this.fb.array([
-          this.fb.group(this.shiftDateGroup)
-      ]),
+        restaurant:   [restaurantID, Validators.required],
+        Type:         ['', Validators.required],
+        position:     ['', Validators.required],
+        description:  ['', Validators.required],
+        payPerHour:   ['', Validators.required],
+        shift_date:   this.fb.array([
+            this.fb.group(this.shiftDateGroup)
+          ]),
       }
     );
   }
+
+  /** Adds Shift date to Array builder */
   addShiftDate(){
     this.shiftsAsArray.push(this.fb.group(this.shiftDateGroup))
   }
 
+  /**
+   * Removes Shift date from Array Builder
+   * @param i 
+   */
   remove(i: number){
     this.shiftsAsArray.removeAt(i);
   }
 
-  private runQuery(id: number) {
+
+  /**
+   * Runs query
+   * @param id 
+   * @returns query 
+   */
+  private runQuery(id: number)
+    : Observable<ApolloQueryResult<{businessUser: BusinessUser}>> {
+    
     return this.apollo
             .watchQuery<{businessUser:BusinessUser}>({
               query: PROFILE_QUERY(id)
@@ -154,12 +198,12 @@ export class AddJobComponent implements OnInit {
             .valueChanges;
    }
 
-
+   /** Runs Add Job Mutation */
    private runMutation(){
      this.runShiftMutation()
      .pipe(
-       switchMap(({data}: any) => {
-         console.log("DATA",data);
+       switchMap(({data}: { data: CreateShiftDateWithPayload }) => {
+  
         if(!data?.createShiftDate?.shiftDate?.id){
           throw new Error('Something went wrong');
         }
@@ -172,17 +216,24 @@ export class AddJobComponent implements OnInit {
         })
        } 
      )).subscribe(
-       (res)=>{
+       ()=>{
+        //TO DO, RESET FORM, MAYBE REDIRECT
         this.didFail = false
        },
-      (err)=>{
+      ()=>{
         this.didFail = true
       }
       );
 
    }
 
-   private runShiftMutation(){
+
+   /**
+    * Runs shift mutation
+    * @returns shift mutation 
+    */
+   private runShiftMutation(): 
+  Observable<FetchResult<CreateShiftDateWithPayload, Record<string, any>, Record<string, any>>>{
     return this.apollo.mutate({
       mutation: SHIFT_MUTATION,
       variables: {
